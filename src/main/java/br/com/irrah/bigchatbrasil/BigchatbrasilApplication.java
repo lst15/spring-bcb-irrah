683184fa758d8cd0289f33889ef26ffe8917e7f6
Leandro <38253462+lst15@users.noreply.github.com>
2023-10-05 10:55:36 -0300

    initializr

---
+++ src/main/java/br/com/irrah/bigchatbrasil/BigchatbrasilApplication.java
@@ -0,0 +1,13 @@
+package br.com.irrah.bigchatbrasil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigchatbrasilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigchatbrasilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/irrah/bigchatbrasil/BigchatbrasilApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.irrah.bigchatbrasil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigchatbrasilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
