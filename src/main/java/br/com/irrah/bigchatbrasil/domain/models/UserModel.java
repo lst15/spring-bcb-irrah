dcd5b04188431054bb8f67f94e3248bb6d2eb78a
Leandro <38253462+lst15@users.noreply.github.com>
2023-10-05 12:32:20 -0300

    added user model

---
+++ src/main/java/br/com/irrah/bigchatbrasil/domain/models/UserModel.java
@@ -0,0 +1,42 @@
+package br.com.irrah.bigchatbrasil.domain.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Table(name = "user")
+@Getter
+@Setter
+@AllArgsConstructor
+public class UserModel {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private String user_uuid;
+
+  @GeneratedValue()
+
+  @Column(name = "user_cpf")
+  private String cpf;
+
+  @Column(name = "user_email")
+  private String email;
+
+  @Column(name = "user_name")
+  private String name;
+
+  @Column(name = "user_phone")
+  private String phone;
+
+  @Column(name = "user_company_cnpj")
+  private String company_cnpj;
+
+  @Column(name = "user_company_name")
+  private String company_name;
+
+}
